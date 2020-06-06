479ecd21ae1153da1510137ae4cb2bce6d1be56d
RidgeyRidge <ridge@davisplanet.com>
2020-06-06 17:44:58 -0400

    scenes in and working

---
+++ block breaker/Assets/Scripts/Ball.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+
+    // config params
+    [SerializeField] Paddle paddle1;
+    [SerializeField] float xPush = 2f;
+    [SerializeField] float yPush = 15f;
+    [SerializeField] AudioClip[] ballSounds;
+
+
+    // state
+    Vector2 paddleToBallVector;
+    bool hasStarted = false;
+
+    // Cached component refrences
+    AudioSource myAudioSource;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleToBallVector = transform.position - paddle1.transform.position;
+        myAudioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnMouseClick();
+        } 
+    }
+
+    private void LaunchOnMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 15f);
+        }
+
+
+    }
+
+    private void LockBallToPaddle()
+        {
+         Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+    transform.position = paddlePos + paddleToBallVector;
+        }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (hasStarted)
+        {
+            AudioClip clip = ballSounds[UnityEngine.Random.Range(0, ballSounds.Length)];
+           myAudioSource.PlayOneShot(clip);
+        }
+    }
+}
---
+++ block breaker/Assets/Scripts/ScriptsBlock.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScriptsBlock : MonoBehaviour
+{
+    [SerializeField] AudioClip breakSound;
+    //  Cached refrence
+    Level level;
+
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.CountBreakableBlocks();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+        Destroy(gameObject);
+    }
+}
