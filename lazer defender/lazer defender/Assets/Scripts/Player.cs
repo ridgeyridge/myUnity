dcdfef974f7e48ff12d9cdeece3d7a2b1f3f7f64
RidgeyRidge <ridge@davisplanet.com>
2020-06-17 13:06:19 -0400

    Player shoots!

--- lazer defender/lazer defender/Assets/Scripts/Player.cs
+++ lazer defender/lazer defender/Assets/Scripts/Player.cs
@@ -6,8 +6,12 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    // configuration parameters
 
     [SerializeField] float movespeed = 10f;
+    [SerializeField] float padding = 1f;
+    [SerializeField] GameObject lazerPrefab;
+    [SerializeField] float projectileSpeed = 10f;
 
     float xMin;
     float xMax;
@@ -20,20 +24,25 @@ public class Player : MonoBehaviour
         SetUpMoveBoundaries();
     }
 
-    private void SetUpMoveBoundaries()
-    {
-        Camera gameCamera = Camera.main;
-        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x;
-        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1,0,0)).x;
-
-        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y;
-        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y;
-    }
 
     // Update is called once per frame
     void Update()
     {
         Move();
+        Fire();
+    }
+
+    private void Fire()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+           GameObject lazer = Instantiate
+                (lazerPrefab,
+                transform.position,
+                Quaternion.identity) as GameObject;
+            lazer.GetComponent<Rigidbody2D>().velocity = new Vector2(0, projectileSpeed);
+
+        }
     }
 
     private void Move()
@@ -46,4 +55,15 @@ public class Player : MonoBehaviour
 
         transform.position = new Vector2(newXPos, newYPos);
     }
+
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
+    }
+
 }
