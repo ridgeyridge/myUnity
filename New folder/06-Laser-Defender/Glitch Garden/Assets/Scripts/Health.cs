d4d55ddaf211c1bcaf55b352eb6b9e49a46feb8f
RidgeyRidge <ridge@davisplanet.com>
2020-07-10 13:00:00 -0400

    I Made It!!!

--- New folder/06-Laser-Defender/Glitch Garden/Assets/Scripts/Health.cs
+++ New folder/06-Laser-Defender/Glitch Garden/Assets/Scripts/Health.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class Health : MonoBehaviour
 {
     [SerializeField] float health = 100f;
+    [SerializeField] GameObject deathVFX;
 
     public void DealDamage(float damage)
     {
         health -= damage;
         if (health <= 0)
         {
+            TriggerDeathVFX();
             Destroy(gameObject);
         }
+    }
+
+    private void TriggerDeathVFX()
+    {
+        if(!deathVFX) { return;  }
+       GameObject deathVFXobject =  Instantiate(deathVFX, transform.position, transform.rotation);
+        Destroy(deathVFXobject, 1f);
     }
 
 }
