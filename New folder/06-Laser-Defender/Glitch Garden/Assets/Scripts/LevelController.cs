cd7446de81de41c2d4e8904f164d0d8ba3be0e05
RidgeyRidge <ridge@davisplanet.com>
2020-08-09 11:33:07 -0400

    Scenes Loading <=)

--- New folder/06-Laser-Defender/Glitch Garden/Assets/Scripts/LevelController.cs
+++ New folder/06-Laser-Defender/Glitch Garden/Assets/Scripts/LevelController.cs
@@ -5,8 +5,16 @@ using UnityEngine;
 public class LevelController : MonoBehaviour
 {
 
+    [SerializeField] GameObject winLabel;
     int numberOfAttackers = 0;
     bool levelTimerFinished = false;
+    [SerializeField] float waitToLoad = 4f;
+
+    private void Start()
+    {
+        winLabel.SetActive(false);
+    }
+
 
     public void AttackerSpawned()
     {
@@ -18,8 +26,16 @@ public class LevelController : MonoBehaviour
         numberOfAttackers--;
         if (numberOfAttackers <= 0 && levelTimerFinished)
         {
-            Debug.Log("End Level Now!");
+            StartCoroutine(HandleWinCondition());
         }
+    }
+
+    IEnumerator HandleWinCondition()
+    {
+        winLabel.SetActive(true);
+        GetComponent<AudioSource>().Play();
+        yield return new WaitForSeconds(waitToLoad);
+        FindObjectOfType<LevelLoader>().LoadNextScene();
     }
 
     public void LevelTimerFinished()
